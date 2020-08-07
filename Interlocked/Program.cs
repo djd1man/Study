ff372f6926ceccd8d0d1aa01e5f488d0ae3b29b2
Dmitry Emelyanov <dmitry.emelyanov@altenar.com>
2020-08-07 10:13:45 +0300

    Add project files.

---
+++ Interlocked/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Interlocked
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
