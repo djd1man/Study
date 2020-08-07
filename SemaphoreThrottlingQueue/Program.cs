5bcd4a9bde09bfb30b4d0bf9933611e88aa4b18e
Dmitry Emelyanov <dmitry.emelyanov@altenar.com>
2020-08-07 13:26:22 +0300

    Semaphore example

---
+++ SemaphoreThrottlingQueue/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SemaphoreThrottlingQueue
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Task.Run(WriteToQueue);
+            await Task.Run(ReadFromQueue);
+        }
+
+        public static void ReadFromQueue()
+        {
+            while (true)
+            {
+                if (_queue.TryDequeue(out var res))
+                {
+                    Console.WriteLine("Dequeue: " + res);
+
+                    _semaphore.Release();
+                }
+
+                Thread.Sleep(1000);
+            }
+        }
+
+        public static void WriteToQueue()
+        {
+            int i = 0;
+            while (true)
+            {
+                _semaphore.WaitOne();
+
+                _queue.Enqueue(i);
+
+                Console.WriteLine($"{i} added to queue");
+
+                i++;
+
+                Thread.Sleep(100);
+            }
+        }
+
+        private static ConcurrentQueue<int> _queue = new ConcurrentQueue<int>();
+
+        private static Semaphore _semaphore = new Semaphore(10, 10);
+    }
+}
