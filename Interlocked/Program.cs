bd2dba8a573c5ec2d0919de07b5073c4d9323a2c
Dmitry Emelyanov <dmitry.emelyanov@altenar.com>
2020-08-07 11:09:56 +0300

    Exclusive access to the resource using intelocked

--- Interlocked/Program.cs
+++ Interlocked/Program.cs
@@ -1,12 +1,74 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 
-namespace Interlocked
+namespace InterlockedProj
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Task.Run(Method1);
+            Task.Run(Method2);
+            Console.ReadKey();
         }
+
+        public static void Method1()
+        {
+            while (true)
+            {
+                _locker.Enter();
+
+                Console.WriteLine($"Method 1: {++_variable}");
+                Thread.Sleep(300);
+
+                _locker.Leave();
+            }
+        }
+        
+        public static void Method2()
+        {
+            while (true)
+            {
+                _locker.Enter();
+
+                Console.WriteLine($"Method 2: {--_variable}");
+                Thread.Sleep(300);
+
+                _locker.Leave();
+            }
+        }
+
+        public static InterlockedSlim _locker = new InterlockedSlim();
+
+        private static int _variable = 0;
+    }
+
+
+    public class InterlockedSlim
+    {
+        public void Enter()
+        {
+            while (true)
+            {
+                if (Interlocked.CompareExchange(
+                    ref _inUse, 1, 0) == 0)
+                {
+                    return;
+                }
+            }
+        }
+
+        public void Leave()
+        {
+            if (Interlocked.CompareExchange(
+                ref _inUse, 0, 1) == 0)
+            {
+                throw new InvalidOperationException("The lock is not entered.");
+            }
+        }
+
+        // 1 - in use, 0 - not in use
+        private int _inUse;
     }
 }
